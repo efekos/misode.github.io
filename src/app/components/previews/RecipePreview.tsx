d2fe3628a145a3d36c2eadd8465923faa617fa65
efekos <hefeolgun10@gmail.com>
2024-03-11 23:16:26 +0300

    add todos

--- src/app/components/previews/RecipePreview.tsx
+++ src/app/components/previews/RecipePreview.tsx
@@ -4,7 +4,7 @@ import CraftingShapedRecipePreview from './recipe/CraftingShapedRecipePreview.js
 import CraftingShapelessRecipePreview from './recipe/CraftingShapelessRecipePreview.jsx'
 import SmeltingRecipePreview from './recipe/SmeltingRecipePreview.jsx'
 
-const hasRecipePreview = ['minecraft:crafting_shaped','minecraft:crafting_shapeless','minecraft:smelting','minecraft:blasting','minecraft:smoking']
+const hasRecipePreview = ['minecraft:crafting_shaped','minecraft:crafting_shapeless','minecraft:smelting','minecraft:blasting','minecraft:smoking','minecraft:campfire_cooking']
 
 export default function RecipePreview({data}: PreviewProps){
 
@@ -18,6 +18,10 @@ export default function RecipePreview({data}: PreviewProps){
 	if(recipeType==='minecraft:smelting') return <SmeltingRecipePreview data={data}/>
 	if(recipeType==='minecraft:blasting') return <SmeltingRecipePreview data={data}/>
 	if(recipeType==='minecraft:smoking') return <SmeltingRecipePreview data={data}/>
+	if(recipeType==='minecraft:campfire_cooking') return <SmeltingRecipePreview data={data}/>
+	//TODO smithing_transform
+	//TODO smithing_trim
+	//TODO stonecutting
 
 	else return <span>Couldn't find preview</span>
 
